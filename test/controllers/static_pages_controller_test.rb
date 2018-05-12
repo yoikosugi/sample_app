b708caeeb861151fec617081c9e5be1fcd28d268
yoshihiko sughimura <yoikosugi@gmail.com>
2018-05-12 14:41:52 +0900

    てすとのこみっとを取り消します

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    # 練習のコメントです
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 

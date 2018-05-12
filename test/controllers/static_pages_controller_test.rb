17d01fcd528b1b59fe658d4f5c133c09597074b3
yoshihiko sughimura <yoikosugi@gmail.com>
2018-05-12 14:41:22 +0900

    テストのコミットです

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
+    # 練習のコメントです
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
